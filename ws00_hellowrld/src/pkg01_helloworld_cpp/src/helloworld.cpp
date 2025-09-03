8ba87c2a070a64ea36f0af74fff0f7d0500af2f0
fengruhua <fffffengruhua@163.com>
2025-09-03 19:32:33 +0800

    chapter1_finally

--- ws00_hellowrld/src/pkg01_helloworld_cpp/src/helloworld.cpp
+++ ws00_hellowrld/src/pkg01_helloworld_cpp/src/helloworld.cpp
@@ -1,6 +1,12 @@
 #include <cstdio>
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode :public rclcpp::Node{
+  public:
+    MyNode():Node("Class_NOde"){
+      RCLCPP_INFO(this->get_logger(),"Hello World MyNode Class");
+    }
+};
 
 int main(int argc, char ** argv)
 {
@@ -9,10 +15,13 @@ int main(int argc, char ** argv)
 
   rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("helloworld_cpp");
+  // 最好不要混着用，实例化和继承
+  // auto node = rclcpp::Node::make_shared("helloworld_cpp");
 
-  RCLCPP_INFO(node->get_logger(), "Hello World, this is INFO!");
-  RCLCPP_WARN(node->get_logger(), "Hello World, this is WARN!");
+  auto node_class = std::make_shared<MyNode>();
+
+  // RCLCPP_INFO(node->get_logger(), "Hello World, this is INFO!");
+  // RCLCPP_WARN(node->get_logger(), "Hello World, this is WARN!");
 
 
   rclcpp::shutdown();
