1a97d49434cbb140e9c92b062c83456bcb08ed1c
fengruhua <fffffengruhua@163.com>
2025-09-05 21:00:25 +0800

    TopicPublish02

--- ws01_plumbing/src/cpp01_topic/src/demo01_talker_str.cpp
+++ ws01_plumbing/src/cpp01_topic/src/demo01_talker_str.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+// 设置定时器时间
 using namespace std::chrono_literals;
 
 class TopicPublish : public rclcpp::Node
@@ -9,26 +10,32 @@ private:
   
 public:
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-
+  rclcpp::TimerBase::SharedPtr timer_;
   size_t count;
   TopicPublish();
   ~TopicPublish();
-  void TimeCallback(int hz);
+  // 创建定时器
+  void TimeCallback();
 };
 
 TopicPublish::TopicPublish():Node("TopicPublish"),count(0)
 {
   RCLCPP_INFO(this->get_logger(), "发布节点创建");
   publisher_ = this->create_publisher<std_msgs::msg::String>("/demo01", 10);
+  timer_ = this->create_wall_timer(1s,std::bind(&TopicPublish::TimeCallback,this));
 }
 
 TopicPublish::~TopicPublish()
 {
 }
 
-void TopicPublish::TimeCallback(int hz)
-{
-
+// 创建定时器
+void TopicPublish::TimeCallback()
+{ 
+  auto msgs = std_msgs::msg::String();
+  msgs.data = "hello world" + std::to_string(count++);
+  publisher_->publish(msgs);
+  RCLCPP_INFO(this->get_logger(), "发布的消息为：%s ",msgs.data.c_str());
 }
 
 int main(int argc, char ** argv)
